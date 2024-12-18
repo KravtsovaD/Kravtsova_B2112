435d7a5f8366749172e1e2a34b89fd51ded9fd48
KravtsovaD <kravtsovad499@gmail.com>
2024-12-18 21:15:15 +0200

    Hw2+visio

---
+++ pythonProject/HW2.py
@@ -0,0 +1,24 @@
+#https://itstepedu-my.sharepoint.com/:u:/g/personal/kravtsova_d_student_itstep_org/EYgGivcMb9JOqVJ-v45ndP8BKFRrl2bg2aPGAGL_Uk66vQ?e=0OGrcV
+
+#1011
+
+a=input()
+print(a[2],a[1],a[0])
+
+#1012
+
+a = int(input())
+a = a**3
+print(a)
+
+#1013
+
+x,y=map(int,input().split())
+print('%.2f'%((x+y)/2))
+
+#1014
+
+a=input()
+print(a[1]+a[2]+a[0])
+
+
