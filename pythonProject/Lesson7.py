8f46491172e29a01f8256d037a04424ad2a96720
b2112 <viacheslav.martson@gmail.com>
2024-12-22 17:51:21 +0200

    Lesson7

--- pythonProject/Lesson7.py
+++ pythonProject/Lesson7.py
@@ -1,4 +1,5 @@
 # import math
+from operator import index
 from random import randint
 #
 # lst = []
@@ -42,3 +43,32 @@ sum_negative = 0
 for num in lst:
     if num < 0: sum_negative += num
 print("Sum negative item list: ", sum_negative)
+
+sum_even = 0
+
+for num in lst:
+    if num % 2 == 0: sum_even +=num
+print("Sum even item list: ", sum_even)
+
+sum_odd = 0
+
+for num in lst:
+    if num % 2 != 0: sum_odd +=num
+print("Sum odd item list: ", sum_odd)
+
+sum_mult3 = 1
+
+for i in range(len(lst)):
+    if i % 3 == 0: sum_mult3 *= lst[i]
+print("Sum mult item list: ", sum_mult3)
+
+mult_range = 1
+min_val_lst = min(lst)
+max_val_lst = max(lst)
+index_min = lst.index(min_val_lst)
+index_max = lst.index(max_val_lst)
+for value in range(index_min, index_max):
+   if index_max < index_min: index_max, index_min =  index_min, index_max
+for i in range(index_min,index_max,1):
+    mult_range *= lst[i]
+print("Multiply between minimum and maximum element: ", mult_range)
